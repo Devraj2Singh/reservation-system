ccba6c972c3e48359e5ec669e196d13ef2a20079
Devraj Singh <devrajs9199@gmail.com>
2026-07-11 16:50:51 +0530

    Configure production socket URL

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://reservation-system-production-31b0.up.railway.app/api");
 
 export default socket;
